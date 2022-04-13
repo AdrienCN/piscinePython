18da63d3799d58d5019de6a9a91e3ff77ebd3ef5
adrien <adrien.constan@gmail.com>
2022-04-13 21:51:17 +0200

    auto-commit

---
+++ day00/ex09/guess.py
@@ -0,0 +1,11 @@
+import sys
+
+print("""Welcome to the \"Guess the number game woop woop\".
+        You have to guess a number between 1 and 99.
+        To exit, enter \"exit\""")
+answer = 0
+attempt = 0
+while answer != result:
+    print("Please enter a number between 1 and 99:")
+
+
