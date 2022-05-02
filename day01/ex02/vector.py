010c332294c539be3a2575ef7bafad71a3e8d5d6
adrien <adrien.constan@gmail.com>
2022-05-02 20:04:50 +0200

    auto-commit

--- day01/ex02/vector.py
+++ day01/ex02/vector.py
@@ -60,13 +60,108 @@ class  Vector:
                i = 0.0
                self.values = []
                while i < size:
-                   self.values.append([i])
+                   self.values.append([float(i)])
                    i += 1.0
                self.shape = (size, 1)
 
+               
+    def __add__(self, other):
+        if not isinstance(other, Vector):
+            raise ValueError("Vector + ", type(other), " not available")
+        if self.shape != other.shape:
+            raise ValueError("Operands [+] Vector have NOT  the same dimension")
+        # ROW add
+        tmp = []
+        if self.shape[0] == 1:
+            for i in range(self.shape[1]):
+                tmp.append(self.values[i] + other.values[i])
+        # COL add
+        else:
+            for i in range(self.shape[0]):
+                tmp.append([self.values[i][0] + other.values[i][0]])
+        return Vector(tmp)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+
+    def __sub__(self, other):
+        if not isinstance(other, Vector):
+            raise ValueError("Vector - ", type(other), " not available")
+        if self.shape != other.shape:
+            raise ValueError("Operands [-] Vector have NOT  the same dimension")
+        # ROW add
+        tmp = []
+        if self.shape[0] == 1:
+            for i in range(self.shape[1]):
+                tmp.append(self.values[i] - other.values[i])
+        # COL add
+        else:
+            for i in range(self.shape[0]):
+                tmp.append([self.values[i][0] - other.values[i][0]])
+        return Vector(tmp)
+
+    def __rsub__(self, other):
+        return self.__sub__(other)
+
+    def __truediv__(self, other):
+        if not check_isnumber(other):
+            raise ValueError("Vector / ", type(other), " not available")
+        if other == 0:
+            raise ValueError("TrueDiv : division by 0 forbidden")
+        tmp = []
+        if self.shape[0] == 1:
+            for i in range(self.shape[1]):
+                tmp.append(self.values[i] / other)
+        else:
+            i = 0
+            while i < self.shape[0]:
+                print("-", self.values[i]) 
+                i += 1
+        return Vector(tmp)
+            
+    def __rtruediv__(self, other):
+        raise ValueError("A scalar cannot be divided by a Vector")
+
+    def __mul__(self, other):
+        if not check_isnumber(other):
+            raise ValueError("Vector * ", type(other), " not available")
+        tmp = []
+        if self.shape[0] == 1:
+            for i in range(self.shape[1]):
+                tmp.append(self.values[i] * other)
+        else:
+            for i in range(self.shape[0]):
+                tmp.append([self.shape[i][0] * other])
+        return Vector(tmp)
+    
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+
+    def __str__(self):
+        return f"Vector shape : row {self.shape[0]} - col {self.shape[1]} | Vector Values = {self.values}"
+
+    def __repr__(self):
+        return f"Vector(shape({self.shape}), values({self.values}))"
+
+        
+ 
+
+class Test:
+    def __init__(self, nb):
+        self.tmp = nb
+
+    def __add__(self, other):
+        return (self.tmp + other.tmp)
+
 foo = Vector(3)
-bar = Vector([3.0, 2.0])
-print("COL values : ", foo.values, " | shape : ", foo.shape)
-print("ROW values : ", bar.values, " | shape : ", bar.shape)
+bar = Vector([[3.0], [2.0], [1.0]])
+print("FOO values : ", foo.values, " | shape : ", foo.shape)
+print("BAR values : ", bar.values, " | shape : ", bar.shape)
+baz = bar + foo
+print("BAZ  values : ", baz.values, " | shape : ", baz.shape)
+tmp = baz / 2.0
 
-
+print(str(baz))
+print(repr(foo))
