11353e324b3683940cce59d811b4152e299ae99b
Adrien CONSTANTIN <adconsta@e3r4p14.clusters.42paris.fr>
2022-05-06 19:08:39 +0200

    auto-commit

--- init_python.sh
+++ init_python.sh
@@ -4,8 +4,10 @@ MYPATH="/goinfre/$USER/miniconda3"
 curl -LO "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
 sh Miniconda3-latest-Linux-x86_64.sh -b -p $MYPATH
 $MYPATH/bin/conda init zsh 
+$MYPATH/bin/conda init bash
 $MYPATH/bin/conda config --set auto_activate_base false
 source ~/.zshrc
+source ~/.bashrc
 conda create --name 42AI-$USER python=3.7 jupyter pandas pycodestyle
+rm Miniconda3-latest-linux-86_64.sh
 conda activate 42AI-$USER
-rm Miniconda-latest-linux-86_64.sh
