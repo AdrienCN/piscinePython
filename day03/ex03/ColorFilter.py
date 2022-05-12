85d057c8422f8c0d1734f6cf2597d717327eb9c7
Adrien CONSTANTIN <adconsta@e3r4p12.clusters.42paris.fr>
2022-05-12 18:22:53 +0200

    auto-commit

--- day03/ex03/ColorFilter.py
+++ day03/ex03/ColorFilter.py
@@ -40,10 +40,22 @@ class ColorFilter:
             and filter != "mean" and filter != "m"):
             return None
         if (filter == "mean" or filter == "m"):
-            gray_arr = np.tile(array, 1)
-            gray_arr[..., 0:1] = gray_arr[..., 0:1] * 0.299
-            gray_arr[..., 1:2] = gray_arr[..., 1:2] * 0.587
-            gray_arr[..., 2:3] = gray_arr[..., 2:3] * 0.114
+           
+            test = np.sum([2, 3, 4])
+            print("Test: ", test)
+            gray_arr = np.tile(array[..., 0:3], 1)
+            gray_arr[:, :, 0:1] = np.sum([0.299 * array[..., 0:1],\
+                                  0.587 * array[..., 1:2],\
+                                  0.114 * array[..., 2:3]])
+
+            gray_arr[:, :, 1:2] = np.sum([0.299 * array[..., 0:1],\
+                                  0.587 * array[..., 1:2],\
+                                  0.114 * array[..., 2:3]])
+
+            gray_arr[:, :, 2:3] = np.sum([0.299 * array[..., 0:1],\
+                                  0.587 * array[..., 1:2],\
+                                  0.114 * array[..., 2:3]])
+
             return gray_arr
         else:
             if len(kwargs) != 3 :
@@ -53,7 +65,7 @@ class ColorFilter:
                 return None
             if sum(lst) != 1:
                 return None
-            gray_arr = np.tile(array, 1)
+            gray_arr = np.tile(array[..., 0:3], 1)
             gray_arr[..., 0:1] = gray_arr[..., 0:1] * lst[0]
             gray_arr[..., 1:2] = gray_arr[..., 1:2] * lst[1]
             gray_arr[..., 2:3] = gray_arr[..., 2:3] * lst[2]
@@ -90,10 +102,10 @@ imp.display(red_arr)
 imp.display(arr)
 """
 # Gray
-gray_arr = cf.to_grayscale(arr, "m", a=0.299, b=0.587, c=0.114)
+gray_arr = cf.to_grayscale(arr, "m")
 imp.display(gray_arr)
 imp.display(arr)
-
+"""
 gray_arr = cf.to_grayscale(arr, "w", a=0.0, b=0.0, c=1.0)
 imp.display(gray_arr)
 imp.display(arr)
@@ -101,4 +113,4 @@ imp.display(arr)
 gray_arr = cf.to_grayscale(arr, "w", a=1.0, b=0.0, c=0.0)
 imp.display(gray_arr)
 imp.display(arr)
-
+"""
