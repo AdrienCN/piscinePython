ddb50456b8f2149f5b3a1771f4da6c442807f354
adrien <adrien.constan@gmail.com>
2022-05-13 16:01:13 +0200

    auto-commit

--- day03/ex03/ColorFilter.py
+++ day03/ex03/ColorFilter.py
@@ -2,6 +2,7 @@ from ImageProcessor import ImageProcessor
 from matplotlib import pyplot
 import numpy as np
 
+
 class ColorFilter:
 
     def __init__(self):
@@ -36,39 +37,40 @@ class ColorFilter:
         if not isinstance(array, np.ndarray):
             return None
         if not isinstance(filter, str)\
-            and (filter != "w" and filter != "weight"\
-            and filter != "mean" and filter != "m"):
+            and (filter != "w" and filter != "weight"
+                 and filter != "mean" and filter != "m"):
             return None
         if (filter == "mean" or filter == "m"):
-            gray_arr = np.tile(array[..., 0:3], 1)
-            gray_arr[:, :, 0:1] = 0.299 * array[..., 0:1]
-            gray_arr[:, :, 1:2] = 0.587 * array[..., 1:2]
-            gray_arr[:, :, 2:3] = 0.114 * array[..., 2:3]
-            print("Arr : ", gray_arr[2,0,0:3])
-            print("SUM :", np.sum(gray_arr[..., 0:3]))
-            gray_arr[..., 0:1] = np.sum(gray_arr[..., 0:3]) / 3
-            gray_arr[..., 1:2] = np.sum(gray_arr[..., 0:3]) / 3
-            gray_arr[..., 2:3] = np.sum(gray_arr[..., 0:3]) / 3
-            return gray_arr
+            r = 0.299
+            g = 0.587
+            b = 0.114
         else:
-            if len(kwargs) != 3 :
+            if len(kwargs) != 3:
                 return None
             lst = list(kwargs.values())
             if not all(isinstance(x, float) for x in lst):
                 return None
             if sum(lst) != 1:
                 return None
-            gray_arr = np.tile(array[..., 0:3], 1)
-            gray_arr[..., 0:1] = gray_arr[..., 0:1] * lst[0]
-            gray_arr[..., 1:2] = gray_arr[..., 1:2] * lst[1]
-            gray_arr[..., 2:3] = gray_arr[..., 2:3] * lst[2]
-            return gray_arr
+            r = lst[0]
+            g = lst[1]
+            b = lst[2]
+        means = [r, g, b]
+        gray_arr = np.tile(array[..., 0:3], 1)
+        gray_arr = gray_arr[..., [0, 1, 2]] * means
+        sum_arr = np.sum(gray_arr, 2)
+        for i in range(array.shape[0]):
+            for j in range(array.shape[1]):
+                gray_arr[i, j, [0, 1, 2]] = [sum_arr[i, j],
+                                             sum_arr[i, j],
+                                             sum_arr[i, j]]
+        return gray_arr
+
 
 imp = ImageProcessor()
 arr = imp.load("elon.png")
 cf = ColorFilter()
 
-"""
 # Original Img
 imp.display(arr)
 
@@ -93,12 +95,12 @@ imp.display(arr)
 red_arr = cf.to_red(arr)
 imp.display(red_arr)
 imp.display(arr)
-"""
+
 # Gray
 gray_arr = cf.to_grayscale(arr, "m")
 imp.display(gray_arr)
 imp.display(arr)
-"""
+
 gray_arr = cf.to_grayscale(arr, "w", a=0.0, b=0.0, c=1.0)
 imp.display(gray_arr)
 imp.display(arr)
@@ -106,4 +108,3 @@ imp.display(arr)
 gray_arr = cf.to_grayscale(arr, "w", a=1.0, b=0.0, c=0.0)
 imp.display(gray_arr)
 imp.display(arr)
-"""
