559c5c924b39d9f38b2cd5673bb988a7f579a373
adrien <adrien.constan@gmail.com>
2022-05-13 19:05:58 +0200

    auto-commit

--- day03/ex00/NumPyCreator.py
+++ day03/ex00/NumPyCreator.py
@@ -3,7 +3,7 @@ import random
 
 
 class NumPyCreator:
-    
+
     def __init__(self):
         pass
 
@@ -15,7 +15,8 @@ class NumPyCreator:
             else:
                 raise TypeError("arg must be a list")
         except Exception as msg:
-            print("Error: from_list :\n", msg)
+            pass
+            # print("Error: from_list :\n", msg)
         return np_arr
 
     def from_tuple(self, tpl):
@@ -26,7 +27,8 @@ class NumPyCreator:
             else:
                 raise TypeError("arg must be a tuple")
         except Exception as msg:
-            print("Error: from_tuple :\n", msg)
+            pass
+            # print("Error: from_tuple :\n", msg)
         return np_arr
 
     def from_iterable(self, itr):
@@ -37,7 +39,8 @@ class NumPyCreator:
             else:
                 raise TypeError("arg must be an iterable")
         except Exception as msg:
-            print("Error: from_iterable :\n", msg)
+            pass
+            # print("Error: from_iterable :\n", msg)
         return np_arr
 
     def from_shape(self, shape, value=0):
@@ -48,7 +51,8 @@ class NumPyCreator:
             else:
                 raise TypeError("shape must be a tuple")
         except Exception as msg:
-            print("Error: from_shape :\n", msg)
+            pass
+            # print("Error: from_shape :\n", msg)
         return np_arr
 
     def random(self, shape):
@@ -59,7 +63,8 @@ class NumPyCreator:
             else:
                 raise TypeError("arg must be a tuple")
         except Exception as msg:
-            print("Error: from_random :\n", msg)
+            pass
+            # print("Error: from_random :\n", msg)
         return np_arr
 
     def identity(self, n):
@@ -70,5 +75,49 @@ class NumPyCreator:
             else:
                 raise TypeError("arg must be an int")
         except Exception as msg:
-            print("Error: from_identity :\n", msg)
+            pass
+            # print("Error: from_identity :\n", msg)
         return np_arr
+
+
+npc = NumPyCreator()
+print("Creating NumpyArray:")
+print("From LIST:")
+arr = npc.from_list([[], []])
+print(arr)
+arr = npc.from_list([[1, 2, 3], [6, 3, 4], [8, 5, 6]])
+print(arr)
+
+print("\nFrom TUPLE:")
+arr = npc.from_tuple(("a", "b", "c"))
+print(arr)
+
+print("\nFrom ITERABLE:")
+arr = npc.from_iterable(range(5))
+print(arr)
+
+print("\nFrom SHAPE:")
+arr = npc.from_shape((0, 0))
+print(arr)
+arr = npc.from_shape((3, 5))
+print(arr)
+
+print("\nSize should be the same but value RANDOM:")
+arr = npc.random((3, 5))
+print("A:\n", arr)
+arr = npc.random((3, 5))
+print("B:\n", arr)
+arr = npc.random((3, 5))
+print("C:\n", arr)
+
+print("\nIdentity:")
+arr = npc.identity(4)
+print(arr)
+
+print("\nError check : None should appear 6 times")
+print(npc.from_list("toto"))
+print(npc.from_list([[1, 2, 3], [6, 3, 4], [8, 5, 6, 7]]))
+print(npc.from_tuple(3.2))
+print(npc.from_tuple(((1, 5, 8), (7, 5))))
+print(npc.from_shape((-1, -1)))
+print(npc.identity(-1))
