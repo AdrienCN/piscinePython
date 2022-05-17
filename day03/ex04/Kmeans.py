e6bd4861185a4ea6661df702d92b17c646db0a71
adrien <adrien.constan@gmail.com>
2022-05-17 19:01:24 +0200

    auto-commit

--- day03/ex04/Kmeans.py
+++ day03/ex04/Kmeans.py
@@ -1,6 +1,9 @@
 import numpy as np
 import csv
+import random
+import copy
 import sys
+from csvreader import CsvReader
 
 
 def check_params(ncentroid, max_iter):
@@ -12,22 +15,161 @@ def check_params(ncentroid, max_iter):
         return 0
     return 1
 
+def get_distance(data_point, centroids):
+    # Pour chaque centroid
+    distance = []
+    ret = 0
+    for i in range(len(centroids)):
+        #dst = np.square((data_point[0] - centroids[i][0])**2 + (data_point[1] - centroids[i][1])**2 + (data_point[2] - centroids[i][2])**2)
+        #dst = np.absolute(data_point[0] - centroids[i][0]) + np.absolute(data_point[1] - centroids[i][1]) + np.absolute(data_point[2] - centroids[i][2])
+        dst = abs(data_point[0] - centroids[i][0]) + abs(data_point[1] - centroids[i][1]) + abs(data_point[2] - centroids[i][2])
+        distance.append(dst)
+        if dst < distance[ret]:
+            ret = i
+    #print("ret : ", ret)
+    return ret
+
+def     get_mean(cluster, X):
+    #Pour chaque data dans mon cluster
+    x = 0.0
+    y = 0.0
+    z = 0.0
+    c_len = len(cluster)
+    if c_len == 0:
+        return [0, 0, 0]
+    for i in range(c_len):
+        x += X[cluster[i]][0]
+        z += X[cluster[i]][1]
+        y += X[cluster[i]][2]
+    x = x / c_len
+    y = y / c_len
+    z = z / c_len
+    return [x, y, z]
+        
+
+class KmeansClustering:
+    def __init__(self, max_iter=20, ncentroid=4):
+        self.ncentroid = ncentroid # number of centroids
+        self.max_iter = max_iter # iteration max to updates centroids
+        self.centroids = [] # centroids values
+        self.clusters = [[] for i in range(self.ncentroid)]
+
+    def get_clusters_means(self, data_arr, X):
+        self.clusters.clear()
+        self.clusters= [[] for i in range(self.ncentroid)]
+        # rempli nos clusters avec les index
+        for i in range(data_arr.shape[0]):
+            self.clusters[int(data_arr[i][0])].append(i)
+            #edge case cluster is empty after update
+
+        means = [[] for i in range(self.ncentroid)]
+        print("\t***************************")
+        for i in range(self.ncentroid):
+            means[i] = get_mean(self.clusters[i], X)
+            print("cluster{}\n-len : {}\n-Means : {}\n {}\n".format(i, len(self.clusters[i]), means[i], self.clusters[i]))
+        print("\t***************************")
+        return means
+
+    def display_result(self):
+        for i in range(self.ncentroid):
+            print("Centroid [{}] : \n- coordinate {}\n- individuals {}.\n"
+                  .format(i, self.centroids[i], len(self.clusters[i])))
+    
+    def centroids_are_equal(self, cluster_mean, centroids):
+        for i in range(self.ncentroid):
+            tmp = list(centroids[i])
+            if cluster_mean[i] != tmp:
+                return 0
+        return 1
+    
+    def fit(self, X):
+        # Get centroids random value from within dataset
+        for i in range(self.ncentroid):
+            index = random.randint(0, X.shape[0] - 1)
+            self.centroids.append(X[index])
+            print("Init centroid {} : {} --- Index {}".format(i, self.centroids[i], index))
+        for i in range(self.max_iter):
+            data_arr = self.predict(X)
+            cluster_mean = self.get_clusters_means(data_arr, X)
+            if self.centroids_are_equal(cluster_mean, self.centroids):
+                self.display_result()
+                exit()
+            self.centroids.clear()
+            self.centroids = copy.deepcopy(cluster_mean)
+        
+        # Update centroid position 
+        #for i in range(self.max_iter):
+        # Create cluster that will be filled with closest datapoint
+            # check si on a trouve le cluster optimum
+            #if (centroids == cluster_predict):
+            #    break
+            #self.centroids.clear()
+            #self.centroids = cluster_predict
+        self.display_result()
+        return
+
+    def predict(self, X):
+        predict = np.zeros((X.shape[0], 1))
+        
+        # Passer dans chaque ligne
+        for i in range(X.shape[0]):
+            #calculer la distance entre ce point et les 4 centroids
+            # retourner l'index du centroids le + proche
+            """
+            index_closest_centroid = get_distance(X[i], self.centroids)
+            clusters[index_closest_centroid].append(i)
+            """
+            predict[i] = get_distance(X[i], self.centroids)
+        count = [0, 0, 0, 0]
+        for i in range(predict.shape[0]):
+            count[int(predict[i][0])] += 1;
+        for i in range(len(count)):
+            print("cluster[{}] : {}".format(i, count[i]))
+        # Calculer la mean value pour chaque centroid et replacer les centroids value par cela
+        """
+        new_centroids = []
+        for i in range(self.ncentroid):
+            print("Cluster_mean : ", i)
+            new_centroids[i] = get_mean(clusters[i], X)
+    
+        for i in range(len(new_centroids)):
+            print("old i = {} : {}\n".format(i ,self.centroids[i]))
+            print("new i = {} : {}\n\n".format(i ,new_centroids[i]))
+        np_centroids = np.zeros((self.ncentroids
+        """
+        return predict
+
+        
+        
+
+
 
 if (len(sys.argv) != 4):
     print("Error : Usage : <file_path.csv> <ncentroid=int> <max_iter=int>")
     exit(1)
 path = sys.argv[1]
+print(path)
 try :
-    with open (path) as csv_file:
-        reader = csv.DictReader(csv_file, ['index', 'height', 'weight', 'bone_density'])
+    with CsvReader(path, ",", True) as csv_file:
+        #reader = csv.DictReader(csv_file, ['index', 'height', 'weight', 'bone_density'])
         #for row in reader:
          #   print("index : {:3} | height {:3.3} | weight {:3.3} | bone {:3.3}".format(row["index"], row["height"], row["weight"], row["bone_density"]))
+        
+        if csv_file == None:
+            print("file could not be opened")
+            exit()
         ncentroid = sys.argv[2].split("=")
         max_iter = sys.argv[3].split("=")
         if not check_params(ncentroid, max_iter):
             raise ValueError("Usage : <ncentroid=int> <max_iter=int>")
-
-
+        ncentroid = int(ncentroid[1])
+        max_iter = int(max_iter[1])
+        data_arr = np.array(csv_file.getdata(), dtype=float)
+        data_arr = np.delete(data_arr, 0, 1)
+        print(data_arr.shape)
+        print(np.ndarray.flatten(data_arr))
+        k = KmeansClustering(max_iter, ncentroid)
+        k.fit(data_arr)
 except Exception as msg:
     print("Error : ", msg)
     exit(1)
---
+++ day03/ex04/csvreader.py
@@ -0,0 +1,96 @@
+import copy
+
+
+class CsvReader():
+
+    def __init__(self, filename=None, sep=",",
+                 header=False, skip_top=0, skip_bottom=0):
+
+        self.open_ok = 0
+        try:
+            self.file = open(filename, "r")
+        except Exception as msg:
+            print("Error cannot open file '{}'".format(filename))
+            return
+        if sep == "":
+            print("Error sep cannot be empty")
+            return
+        self.header = header
+        self.file_header = None
+        self.skip_top = skip_top
+        self.skip_bottom = skip_bottom
+        self.sep = sep
+        self.record_nb = 0
+        self.field_nb = None
+        self.data = []
+        self.open_ok = 1
+
+    def __enter__(self):
+
+
+        if not self.open_ok:
+            return None
+        first_loop = 1
+        count = 0
+        for line in self.file.readlines():
+            if first_loop:
+                if self.header:
+                    self.file_header = line.strip('\n')
+                self.field_nb = line.count(self.sep) + 1
+            self.data.append((line.strip('\n')).split(self.sep))
+
+            # Check absence de chaine vide
+            for i in self.data[-1]:
+                if len(i) <= 0 and not first_loop :
+                    self.data = None
+                    return None
+            if len(self.data[-1]) != self.field_nb:
+                self.data = None
+                return None
+            first_loop = 0
+        self.record_nb = len(self.data)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if value or traceback or type:
+            print("Error caught: {} : {} : {}".format(type , value, traceback))
+        if self.open_ok:
+            self.file.close()
+        return False
+
+    def getdata(self):
+        """
+            Retrieves the data/records from skip_top to skip bottom.
+            Returns:
+            nested list (list(list, list, ...)) representing the data.
+        """
+        tmp_data = copy.deepcopy(self.data)
+        tmp_top = self.skip_top
+        tmp_bottom = self.skip_bottom
+        tmp_record_nb = self.record_nb
+        # remove le header de la liste
+        if self.header:
+            del tmp_data[0]
+
+        if tmp_top > 0 or tmp_bottom > 0:
+            while tmp_record_nb and tmp_top > 0:
+                del tmp_data[0]
+                tmp_record_nb -= 1
+                tmp_top -= 1
+            while tmp_record_nb and tmp_bottom > 0:
+                del tmp_data[-1]
+                tmp_record_nb -= 1
+                tmp_bottom -= 1
+        return tmp_data
+
+    def getheader(self):
+        """
+            Retrieves the header from csv file.
+            Returns:
+            list: representing the data (when self.header is True).
+            None: (when self.header is False)
+        """
+        if self.header is True:
+            return self.file_header
+        else:
+            return None
