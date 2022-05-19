ef20fcbe9c001baad33d522d8c5b6b32221e30a9
adrien <adrien.constan@gmail.com>
2022-05-19 18:06:48 +0200

    auto-commit

--- day04/ex04/SpatioTemporalData.py
+++ day04/ex04/SpatioTemporalData.py
@@ -7,9 +7,10 @@ class SpatioTemporalData:
     def when(self, city):
         year = self.df[['City', 'Year']]
         year = year.loc[(year['City'] == city)]['Year']
-        year_list = []
-        year = year.to_list()
-        [year_list.append(x) for x in year if x not in year_list]
+        #year_list = []
+        #year = year.to_list()
+        #[year_list.append(x) for x in year if x not in year_list]
+        year_list = year.drop_duplicates()
         return year_list
 
     def where(self, year):
