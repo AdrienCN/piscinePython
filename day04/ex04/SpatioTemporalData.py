4e3c9dc218101faa1bdc76af7cccf6cf80423bee
adrien <adrien.constan@gmail.com>
2022-05-19 18:05:30 +0200

    auto-commit

---
+++ day04/ex04/SpatioTemporalData.py
@@ -0,0 +1,26 @@
+from FileLoader import FileLoader
+
+class SpatioTemporalData: 
+    def __init__(self, dataframe):
+        self.df = dataframe
+
+    def when(self, city):
+        year = self.df[['City', 'Year']]
+        year = year.loc[(year['City'] == city)]['Year']
+        year_list = []
+        year = year.to_list()
+        [year_list.append(x) for x in year if x not in year_list]
+        return year_list
+
+    def where(self, year):
+        city = self.df.loc[(self.df['Year'] == year)]['City']
+        return(city.to_list()[0])
+
+
+loader = FileLoader()
+data = loader.load("athlete_events.csv")
+
+sp = SpatioTemporalData(data)
+print(sp.where(2016))
+print(sp.when("Paris"))
+
