756163abdbaeedfb9107fad8d3cc5aa509da9032
adrien <adrien.constan@gmail.com>
2022-05-19 15:02:10 +0200

    auto-commit

---
+++ day04/ex00/FileLoader.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+class FileLoader():
+    
+    def __init__(self):
+        pass
+
+    def load(self, path):
+        try:
+            df = pd.read_csv(path)
+            print("Loading dataset of dimensions {}x{}".format(len(df), len(df.columns)))
+            return df
+        except Exception as msg:
+            print(msg)
+            return None
+
+    def display(df, n):
+        try:
+            if n > 0:
+                df.head(n)
+            else:
+                df.tail(abs(n))
+        except Exception as msg:
+            print(msg)
+            return None
