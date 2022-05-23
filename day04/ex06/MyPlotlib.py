5a1c953a7e6f3aa305a9326d80d6da0f46ec1b5f
Adrien CONSTANTIN <adconsta@e3r3p17.clusters.42paris.fr>
2022-05-23 18:24:56 +0200

    auto-commit

--- day04/ex06/MyPlotlib.py
+++ day04/ex06/MyPlotlib.py
@@ -4,9 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from FileLoader import FileLoader
 import seaborn as sns
-#import scipy as sp
 
 def wrong_params(data, features):
+    print("Params check ....")
     try:
         if not isinstance(features, list):
             raise TypeError("Error : Features must be a list")
@@ -15,7 +15,8 @@ def wrong_params(data, features):
             if not isinstance(tmp.min(), (float, int)):
                 raise TypeError("Error : Features \"{}\" is not numerical"
                                 .format(var))
-                return 0
+        print("Params ok !")
+        return 0
     except Exception as msg:
         if type(msg) == KeyError:
             print("Key Error: {} is not a valid Dataframe column".format(msg))
@@ -35,41 +36,45 @@ class MyPlotLib:
         for var in features:
             data_var = data[['ID', var]].drop_duplicates().dropna()
             data_var = data_var[var]
-            print(var)
             plt.hist(data_var,
                      range=[data_var.min(), data_var.max()],
-                     edgecolor='black')
-            plt.legend(title=var)
+                     edgecolor='black', label=var)
+            plt.legend()
             plt.show()
         return 0
 
     def density(self, data, features):
         if wrong_params(data, features):
             return 1
-        kde_list = []
-        label_list = []
         for var in features:
             data_var = data[['ID', var]].drop_duplicates().dropna()
             data_var = data_var[var]
-            kde_list.append(data_var)
-            label_list.append(var)
-            """
-            plt.hist(data_var, range=[data_var.min(), data_var.max()], density=True)
-            plt.legend(var)
-            plt.show()
-            """
-        plt.figure()
-        for i in range(len(kde_list)):
-            sns.kdeplot(kde_list[i], label=label_list[i])
-        plt.legend(labels=label_list)
+            sns.kdeplot(data_var, label=var)
+        plt.legend()
         plt.show()
         return 0
 
     def pair_plot(self, data, features):
-        pass
+        if wrong_params(data, features):
+            return 1
+        #seaborn
+        sns.pairplot(data[features])
+        """
+        #matplotlib
+        to_plot = data[features]
+        to_plot.plot.scatter(features[0], features[1])
+        to_plot.plot.scatter(features[1], features[0])
+        """
+        plt.show()
+      
 
-    def box_plit(self, data, features):
-        pass
+    def box_plot(self, data, features):
+        if wrong_params(data, features):
+            return 1
+        print("Box plot ...")
+        to_plot = data[features]
+        to_plot.boxplot()
+        plt.show()
 
 if len(sys.argv) !=  1:
     sys.exit("Usage : python3 <exec_file>")
@@ -78,3 +83,5 @@ data = loader.load("athlete_events.csv")
 myplot = MyPlotLib()
 myplot.histogram(data, ['Height', 'Weight'])
 myplot.density(data, ['Height', 'Weight'])
+myplot.pair_plot(data, ['Height', 'Weight'])
+myplot.box_plot(data, ['Height', 'Weight'])
