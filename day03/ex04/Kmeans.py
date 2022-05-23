b79e826c787985e9f40fc86de516b40bd4b42e1e
Adrien CONSTANTIN <adconsta@e3r3p17.clusters.42paris.fr>
2022-05-23 16:03:14 +0200

    auto-commit

--- day03/ex04/Kmeans.py
+++ day03/ex04/Kmeans.py
@@ -30,19 +30,12 @@ def get_distance(data_point, centroids, min_list, max_list):
         x_norm_2 = (centroids[i][0] - min_list[0]) / x_range
         y_norm_2 = (centroids[i][1] - min_list[1]) / y_range
         z_norm_2 = (centroids[i][2] - min_list[2]) / z_range
-        #dst = (abs(x_norm_1 - x_norm_2) +
-        #       abs(y_norm_1 - y_norm_2) +
-        #       abs(z_norm_1 - z_norm_2))
-        
         dst = np.square((x_norm_1 - x_norm_2)**2 +
                         (y_norm_1 - y_norm_2)**2 +
                         (z_norm_1 - z_norm_2)**2)
-        #dst = np.square((data_point[0] - centroids[i][0])**2 + (data_point[1] - centroids[i][1])**2 + (data_point[2] - centroids[i][2])**2)
-        #dst = abs(data_point[0] - centroids[i][0]) + abs(data_point[1] - centroids[i][1]) + abs(data_point[2] - centroids[i][2])
         distance.append(dst)
         if dst < distance[ret]:
             ret = i
-    #print("ret : ", ret)
     return ret
 
 def     get_mean(cluster, X):
@@ -79,22 +72,18 @@ class KmeansClustering:
         for i in range(self.ncentroid):
             index = random.randint(0, X.shape[0] - 1)
             self.centroids.append(X[index].tolist())
-        for i in range(self.max_iter):
-            #for j in range(self.ncentroid):
-                #print("Round {} centroid {} : {}".format(i, j, self.centroids[j]))
+        prev_centroids = []
+        for iteration in range(self.max_iter):
             data_arr = self.predict(X)
-            cluster_mean = self.get_clusters_means(data_arr, X)
-            if self.centroids_are_equal(cluster_mean, self.centroids):
-                self.display_result()
-                return
-            self.centroids.clear()
-            self.centroids = copy.deepcopy(cluster_mean)
+            new_centroids = self.get_clusters_means(data_arr, X)
+            if np.array_equal(np.array(new_centroids), np.array(self.centroids)):
+                break
+            self.centroids = new_centroids
         self.display_result()
         return
 
     def predict(self, X):
         predict = np.zeros((X.shape[0], 1))
-        # Passer dans chaque ligne
         for i in range(X.shape[0]):
             predict[i] = get_distance(X[i], self.centroids,
                                       self.min_list, self.max_list)
@@ -106,33 +95,22 @@ class KmeansClustering:
     def get_clusters_means(self, data_arr, X):
         self.clusters.clear()
         self.clusters= [[] for i in range(self.ncentroid)]
-        # rempli nos clusters avec les index
         for i in range(data_arr.shape[0]):
             self.clusters[int(data_arr[i][0])].append(i)
-            #edge case cluster is empty after update
         means = [[] for i in range(self.ncentroid)]
         for i in range(self.ncentroid):
             means[i] = get_mean(self.clusters[i], X)
-            #print("cluster{}\n-len : {}\n-Means : {}\n {}\n".format(i, len(self.clusters[i]), means[i], self.clusters[i]))
-        #print("\t***************************")
         return means
 
     def display_result(self):
-        for i in range(self.ncentroid):
-            print("Centroid [{}] : \n- coordinate {}\n- individuals {}.\n"
-                  .format(i, self.centroids[i], len(self.clusters[i])))
-        #get max height
         if (self.ncentroid == 4):
             belt = max(x[0] for x in self.centroids)
             earth = min(x[0] for x in self.centroids)
-            print("Belt {}, Earth {}".format(belt, earth))
             for i in range(len(self.centroids)):
                 if earth in self.centroids[i]:
                     earth = i
                 elif belt in self.centroids[i]:
                     belt = i
-
-            print("Belt {}, Earth {} | tmp :".format(belt, earth))
             venus = -1
             mars = -1
             for i in range(len(self.centroids)):
@@ -143,7 +121,6 @@ class KmeansClustering:
                         venus = i
                     else:
                         mars = i
-
             if self.centroids[venus][0] > self.centroids[earth][0]\
                 and self.centroids[venus][1] < self.centroids[earth][1]:
                     pass
@@ -162,14 +139,6 @@ class KmeansClustering:
                   "-Venus  : K[{}] : val {}   : {}\n"
                   .format(venus, self.centroids[venus], len(self.clusters[venus])))
     
-    def centroids_are_equal(self, cluster_mean, centroids):
-        for i in range(self.ncentroid):
-            tmp = list(centroids[i])
-            if cluster_mean[i] != tmp:
-                return 0
-        print("All centroids are equal : RETURNNG")
-        return 1
-
 def plot_res_3d(X, kmeans):
     
     predict = kmeans.predict(X)
@@ -193,13 +162,8 @@ if (len(sys.argv) != 4):
     print("Error : Usage : <file_path.csv> <ncentroid=int> <max_iter=int>")
     exit(1)
 path = sys.argv[1]
-print(path)
 try :
     with CsvReader(path, ",", True) as csv_file:
-        #reader = csv.DictReader(csv_file, ['index', 'height', 'weight', 'bone_density'])
-        #for row in reader:
-         #   print("index : {:3} | height {:3.3} | weight {:3.3} | bone {:3.3}".format(row["index"], row["height"], row["weight"], row["bone_density"]))
-        
         if csv_file == None:
             print("file could not be opened")
             exit()
@@ -213,7 +177,7 @@ try :
         data_arr = np.delete(data_arr, 0, 1)
         k = KmeansClustering(max_iter, ncentroid)
         k.fit(data_arr)
-        #plot_res_3d(data_arr, k)
+        plot_res_3d(data_arr, k)
 except Exception as msg:
     print("Error : ", msg)
     exit(1)
