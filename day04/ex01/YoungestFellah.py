cfe9b88271871d946b9c548e9c6cffde91ecb012
Adrien CONSTANTIN <adconsta@e3r3p18.clusters.42paris.fr>
2022-05-24 15:50:32 +0200

    auto-commit

--- day04/ex01/YoungestFellah.py
+++ day04/ex01/YoungestFellah.py
@@ -1,6 +1,10 @@
 def youngestfellah(df, year):
-    sub_df = df[['Sex', 'Age', 'Year']]
-    res = sub_df.loc[sub_df['Year'] == year]
-    male_min = res.loc[res['Sex'] == 'M'].min()
-    female_min = res.loc[res['Sex'] == 'F'].min()
-    return ({'f': female_min.Age, 'm': male_min.Age})
+    try:
+        sub_df = df[['Sex', 'Age', 'Year']]
+        res = sub_df.loc[sub_df['Year'] == year]
+        male_min = res.loc[res['Sex'] == 'M'].min()
+        female_min = res.loc[res['Sex'] == 'F'].min()
+        return ({'f': female_min.Age, 'm': male_min.Age})
+    except Exception as msg:
+        print(msg)
+        return None
--- day04/ex01/main.py
+++ day04/ex01/main.py
@@ -2,14 +2,22 @@ from FileLoader import FileLoader
 from YoungestFellah import youngestfellah
 import sys
 
-if len(sys.argv) != 3:
-    print("Usage : python3 main.py <path_to_csv_file> <year>")
-    quit()
-
-path = sys.argv[1]
 loader = FileLoader()
-data = loader.load(path)
-if data is None:
-    quit()
-res = youngestfellah(data, int(sys.argv[2]))
-print("Dict : ", res)
+data = loader.load("athlete_events.csv")
+try:
+    print("Expected output is:\n{'f': 12.0, 'm': 11.0}")
+    print(youngestfellah(data, 1992))
+
+    print("\nExpected output is:\n{'f': 13.0, 'm': 14.0}")
+    print(youngestfellah(data, 2004))
+
+    print("\nExpected output is:\n{'f': 15.0, 'm': 15.0}")
+    print(youngestfellah(data, 2010))
+
+    print("\nExpected output is:\n{'f': nan, 'm': nan}")
+    print(youngestfellah(data, 2003))
+    
+    sys.exit(0)
+except Exception as msg:
+    print(msg)
+    sys.exit(1)
